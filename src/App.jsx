585a4b952622a1379dfa84081a2432a0b5d7e97b
Hugo <hugomdmg@profesorestrazos.net>
2026-06-12 18:47:04 +0200

    fix conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,12 @@ function App() {
     if (turno == "X") setTurno("O");
     if (turno == "O") setTurno("X");
     setWinner(
-      // (values[0][0] != " " &&
-      //   values[0][1] != " " &&
-      //   values[0][2] != " " &&
-      //   values[1][0] != " " &&
-      //   values[1][1] != " " &&
-      //   values[1][2] != " " &&
-      //   values[2][0] != " " &&
-      //   values[2][1] != " " &&
-      //   values[2][2] != " ") &&
-        (values[0][0] == values[0][1] && values[0][0] == values[0][2]) ||
-        (values[1][0] == values[1][1] && values[1][0] == values[1][2]) ||
-        (values[2][0] == values[2][1] && values[2][0] == values[2][2]) ||
-        (values[0][0] == values[1][0] && values[0][0] == values[2][0]) ||
-        (values[1][0] == values[1][1] && values[1][0] == values[2][1]) ||
-        (values[0][2] == values[1][2] && values[0][2] == values[2][2]),
+        (values[0][0] != " " && values[0][0] == values[0][1] && values[0][0] == values[0][2]) ||
+        (values[1][0] != " " && values[1][0] == values[1][1] && values[1][0] == values[1][2]) ||
+        (values[2][0] != " " && values[2][0] == values[2][1] && values[2][0] == values[2][2]) ||
+        (values[0][0] != " " && values[0][0] == values[1][0] && values[0][0] == values[2][0]) ||
+        (values[1][0] != " " && values[1][0] == values[1][1] && values[1][0] == values[2][1]) ||
+        (values[0][2] != " " && values[0][2] == values[1][2] && values[0][2] == values[2][2])
     );
   }
 
